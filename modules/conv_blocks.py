22b5ce7f132792a3aed6f81f48b7bf3a3b9e0eeb
Karabo <mailakarabo3@gmail.com>
2024-07-07 18:07:04 +0200

    format code

--- modules/conv_blocks.py
+++ modules/conv_blocks.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn
-from modules import se_modules as se
 import torch.nn.functional as F
 import torch.nn.init as init
+
+from modules import se_modules as se
+
 
 class GenericBlock(nn.Module):
     """
@@ -24,28 +26,30 @@ class GenericBlock(nn.Module):
     def __init__(self, params, se_block_type=None):
         super(GenericBlock, self).__init__()
         if se_block_type == se.SELayer.CSE.value:
-            self.SELayer = se.ChannelSpatialSELayer(params['num_filters'])
+            self.SELayer = se.ChannelSpatialSELayer(params["num_filters"])
 
         elif se_block_type == se.SELayer.SSE.value:
-            self.SELayer = se.SpatialSELayer(params['num_filters'])
+            self.SELayer = se.SpatialSELayer(params["num_filters"])
 
         elif se_block_type == se.SELayer.CSSE.value:
-            self.SELayer = se.ChannelSpatialSELayer(params['num_filters'])
+            self.SELayer = se.ChannelSpatialSELayer(params["num_filters"])
         else:
             self.SELayer = None
-        padding_h = int((params['kernel_h'] - 1) / 2)
-        padding_w = int((params['kernel_w'] - 1) / 2)
-        self.out_channel = params['num_filters']
-        self.conv = nn.Conv2d(in_channels=params['num_channels'], out_channels=params['num_filters'],
-                              kernel_size=(
-                                  params['kernel_h'], params['kernel_w']),
-                              padding=(padding_h, padding_w),
-                              stride=params['stride_conv'])
+        padding_h = int((params["kernel_h"] - 1) / 2)
+        padding_w = int((params["kernel_w"] - 1) / 2)
+        self.out_channel = params["num_filters"]
+        self.conv = nn.Conv2d(
+            in_channels=params["num_channels"],
+            out_channels=params["num_filters"],
+            kernel_size=(params["kernel_h"], params["kernel_w"]),
+            padding=(padding_h, padding_w),
+            stride=params["stride_conv"],
+        )
         self.prelu = nn.PReLU()
-        self.batchnorm = nn.BatchNorm2d(num_features=params['num_filters'])
-        if params['drop_out'] > 0:
+        self.batchnorm = nn.BatchNorm2d(num_features=params["num_filters"])
+        if params["drop_out"] > 0:
             self.drop_out_needed = True
-            self.drop_out = nn.Dropout2d(params['drop_out'])
+            self.drop_out = nn.Dropout2d(params["drop_out"])
         else:
             self.drop_out_needed = False
 
@@ -97,7 +101,10 @@ class SDnetEncoderBlock(GenericBlock):
     def __init__(self, params, se_block_type=None):
         super(SDnetEncoderBlock, self).__init__(params, se_block_type)
         self.maxpool = nn.MaxPool2d(
-            kernel_size=params['pool'], stride=params['stride_pool'], return_indices=True)
+            kernel_size=params["pool"],
+            stride=params["stride_pool"],
+            return_indices=True,
+        )
 
     def forward(self, input, weights=None):
         """Forward pass
@@ -145,11 +152,11 @@ class SDnetDecoderBlock(GenericBlock):
     def __init__(self, params, se_block_type=None):
         super(SDnetDecoderBlock, self).__init__(params, se_block_type)
         self.unpool = nn.MaxUnpool2d(
-            kernel_size=params['pool'], stride=params['stride_pool'])
+            kernel_size=params["pool"], stride=params["stride_pool"]
+        )
         # self.conv1 = nn.Conv2d(in_channels=params['num_channels'], out_channels=params['num_filters'],
         #                       kernel_size=(1,1),
         #                       stride=params['stride_conv'])
-
 
     def forward(self, input, out_block=None, indices=None, weights=None):
         """Forward pass
@@ -167,7 +174,7 @@ class SDnetDecoderBlock(GenericBlock):
         """
 
         # unpool = self.unpool(input, indices) # , out_block.shape)
-        unpool = F.interpolate(input, scale_factor=2, mode='nearest')
+        unpool = F.interpolate(input, scale_factor=2, mode="nearest")
         # unpool = self.conv1(unpool)
         if out_block is not None:
             concat = torch.cat((out_block, unpool), dim=1)
@@ -180,6 +187,7 @@ class SDnetDecoderBlock(GenericBlock):
         if self.drop_out_needed:
             out_block = self.drop_out(out_block)
         return out_block
+
 
 class ClassifierBlock(nn.Module):
     """
@@ -203,7 +211,11 @@ class ClassifierBlock(nn.Module):
     def __init__(self, params):
         super(ClassifierBlock, self).__init__()
         self.conv = nn.Conv2d(
-            params['num_channels'], params['num_class'], params['kernel_c'], params['stride_conv'])
+            params["num_channels"],
+            params["num_class"],
+            params["kernel_c"],
+            params["stride_conv"],
+        )
 
     def forward(self, input, weights=None):
         """Forward pass
@@ -226,7 +238,6 @@ class ClassifierBlock(nn.Module):
 
 
 class AddCoords(nn.Module):
-
     def __init__(self, with_r=False):
         super().__init__()
         self.with_r = with_r
@@ -250,24 +261,30 @@ class AddCoords(nn.Module):
         xx_channel = xx_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
         yy_channel = yy_channel.repeat(batch_size, 1, 1, 1).transpose(2, 3)
 
-        ret = torch.cat([
-            input_tensor,
-            xx_channel.type_as(input_tensor),
-            yy_channel.type_as(input_tensor)], dim=1)
+        ret = torch.cat(
+            [
+                input_tensor,
+                xx_channel.type_as(input_tensor),
+                yy_channel.type_as(input_tensor),
+            ],
+            dim=1,
+        )
 
         if self.with_r:
-            rr = torch.sqrt(torch.pow(xx_channel.type_as(input_tensor) - 0.5, 2) + torch.pow(yy_channel.type_as(input_tensor) - 0.5, 2))
+            rr = torch.sqrt(
+                torch.pow(xx_channel.type_as(input_tensor) - 0.5, 2)
+                + torch.pow(yy_channel.type_as(input_tensor) - 0.5, 2)
+            )
             ret = torch.cat([ret, rr], dim=1)
 
         return ret
 
 
 class CoordConv(nn.Module):
-
     def __init__(self, in_channels, out_channels, with_r=False, **kwargs):
         super().__init__()
         self.addcoords = AddCoords(with_r=with_r)
-        in_size = in_channels+2
+        in_size = in_channels + 2
         if with_r:
             in_size += 1
         self.conv = nn.Conv2d(in_size, out_channels, **kwargs)
@@ -279,7 +296,16 @@ class CoordConv(nn.Module):
 
 
 class AF(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, bias=False):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding=0,
+        groups=1,
+        bias=False,
+    ):
         super(AF, self).__init__()
         self.out_channels = out_channels
         self.kernel_size = kernel_size
@@ -287,10 +313,16 @@ class AF(nn.Module):
         self.padding = padding
         self.groups = groups
 
-        assert self.out_channels % self.groups == 0, "out_channels should be divided by groups. (example: out_channels: 40, groups: 4)"
+        assert (
+            self.out_channels % self.groups == 0
+        ), "out_channels should be divided by groups. (example: out_channels: 40, groups: 4)"
 
-        self.rel_h = nn.Parameter(torch.randn(out_channels // 2, 1, 1, kernel_size, 1), requires_grad=True)
-        self.rel_w = nn.Parameter(torch.randn(out_channels // 2, 1, 1, 1, kernel_size), requires_grad=True)
+        self.rel_h = nn.Parameter(
+            torch.randn(out_channels // 2, 1, 1, kernel_size, 1), requires_grad=True
+        )
+        self.rel_w = nn.Parameter(
+            torch.randn(out_channels // 2, 1, 1, 1, kernel_size), requires_grad=True
+        )
 
         self.key_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias)
         self.query_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias)
@@ -307,29 +339,43 @@ class AF(nn.Module):
         k_out = self.key_conv(padded_x)
         v_out = padded_v
 
-        k_out = k_out.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride)
-        v_out = v_out.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride)
-
+        k_out = k_out.unfold(2, self.kernel_size, self.stride).unfold(
+            3, self.kernel_size, self.stride
+        )
+        v_out = v_out.unfold(2, self.kernel_size, self.stride).unfold(
+            3, self.kernel_size, self.stride
+        )
 
         k_out_h, k_out_w = k_out.split(self.out_channels // 2, dim=1)
         k_out = torch.cat((k_out_h + self.rel_h, k_out_w + self.rel_w), dim=1)
 
-        k_out = k_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, -1) # -1 = k*k
-        v_out = v_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, -1)
+        k_out = k_out.contiguous().view(
+            batch, self.groups, self.out_channels // self.groups, height, width, -1
+        )  # -1 = k*k
+        v_out = v_out.contiguous().view(
+            batch, self.groups, self.out_channels // self.groups, height, width, -1
+        )
 
-
-        q_out = q_out.view(batch, self.groups, self.out_channels // self.groups, height, width, 1)
+        q_out = q_out.view(
+            batch, self.groups, self.out_channels // self.groups, height, width, 1
+        )
 
         out = q_out * k_out
         out = F.softmax(out, dim=-1)
-        out = torch.einsum('bnchwk,bnchwk -> bnchw', out, v_out).view(batch, -1, height, width)
+        out = torch.einsum("bnchwk,bnchwk -> bnchw", out, v_out).view(
+            batch, -1, height, width
+        )
 
         return out
 
     def reset_parameters(self):
-        init.kaiming_normal_(self.key_conv.weight, mode='fan_out', nonlinearity='relu')
-        init.kaiming_normal_(self.value_conv.weight, mode='fan_out', nonlinearity='relu')
-        init.kaiming_normal_(self.query_conv.weight, mode='fan_out', nonlinearity='relu')
+        init.kaiming_normal_(self.key_conv.weight, mode="fan_out", nonlinearity="relu")
+        init.kaiming_normal_(
+            self.value_conv.weight, mode="fan_out", nonlinearity="relu"
+        )
+        init.kaiming_normal_(
+            self.query_conv.weight, mode="fan_out", nonlinearity="relu"
+        )
 
         init.normal_(self.rel_h, 0, 1)
         init.normal_(self.rel_w, 0, 1)
